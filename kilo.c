03c46e0d6ce1ac10a938d18916166c97d0ffcff1
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-12-10 17:20:09 +0530

    Step 114: Reset dirty

--- kilo.c
+++ kilo.c
@@ -279,6 +279,7 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave() {
@@ -291,6 +292,7 @@ void editorSave() {
       if (write(fd, buf, len) == len) {
         close(fd);
         free(buf);
+        E.dirty = 0;
         editorSetStatusMessage("%d bytes written to disk", len);
         return;
       }
