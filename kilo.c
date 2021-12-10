177b439a12c78b5e5d07eb1ecf6bab58f33e7edf
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-12-10 14:43:00 +0530

    Step 79: Add moving right at end of line

--- kilo.c
+++ kilo.c
@@ -285,7 +285,6 @@ void editorRefreshScreen() {
 
 void editorMoveCursor(int key) {
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-  
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -298,6 +297,9 @@ void editorMoveCursor(int key) {
     case ARROW_RIGHT:
       if (row && E.cx < row->size) {
         E.cx++;
+      } else if (row && E.cx == row->size) {
+        E.cy++;
+        E.cx = 0;
       }
       break;
     case ARROW_UP:
@@ -311,14 +313,12 @@ void editorMoveCursor(int key) {
       }
       break;
   }
-  
   row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   int rowlen = row ? row->size : 0;
   if (E.cx > rowlen) {
     E.cx = rowlen;
   }
 }
-
 
 void editorProcessKeypress() {
   int c = editorReadKey();
