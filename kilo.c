7a619177662c303b29d4a27ff45f5c0b5f6b8e3a
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-12-10 10:19:07 +0530

    Step 50: The Page Up and Page Down keys

--- kilo.c
+++ kilo.c
@@ -16,10 +16,12 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
-ARROW_LEFT = 1000,
-ARROW_RIGHT,
-ARROW_UP,
-ARROW_DOWN
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -68,19 +70,26 @@ int editorReadKey() {
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) die("read");
   }
-  
   if (c == '\x1b') {
     char seq[3];
-    
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
-    
     if (seq[0] == '[') {
-      switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+        }
       }
     }
     return '\x1b';
